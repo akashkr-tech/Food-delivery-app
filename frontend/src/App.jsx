b813d35b1b29c8ba61938723ea7d6d91ab892398
Akash <artist78887@gmail.com>
2025-10-16 08:57:54 +0530

    add some properties in navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
      <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path = '/cart' element = {<Cart/>}/>
+      <Route path = '/order' element = {<PlaceOrder/>}/>
+     </Routes>
     </div>
   )
 }

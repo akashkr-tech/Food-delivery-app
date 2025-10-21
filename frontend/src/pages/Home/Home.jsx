710c7ade40d51e789cc5c34a69c408f99179a432
Akash <artist78887@gmail.com>
2025-10-21 21:13:25 +0530

    debugging

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,12 +4,12 @@ import Header from '../../components/Header/Header.jsx'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.jsx'
 
 const Home = () => {
-  const [category,setCstegory] = useState("All");
+  const [category,setCategory] = useState("All");
   
   return (
     <div>
       <Header/>
-      <ExploreMenu category={category} setCstegory={setCstegory} />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   )
 }

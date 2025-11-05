9d22810b3b0e054a14932f95d0e49c42f463b6aa
Akash <artist78887@gmail.com>
2025-11-05 10:14:14 +0530

    backend work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,13 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
+import StoreContextProvider from "./context/StoreContext";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
-    <>
+    <StoreContextProvider>
       {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
@@ -22,7 +23,7 @@ const App = () => {
         </Routes>
       </div>
       <Footer />
-    </>
+    </StoreContextProvider>
   );
 };
 

03cb5b0cb07e686ec91cdd4d168ec9270511b56f
Akash <artist78887@gmail.com>
2025-11-21 09:51:22 +0530

    solve payment failed issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { Route, Routes,useLocation } from "react-router-dom";
+import { Route, Routes, } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
@@ -11,13 +11,13 @@ import Verify from "./pages/Verify/Verify";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
-  const location = useLocation();
+  // const location = useLocation();
 
   return (
     <StoreContextProvider>
       {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
-        {location.pathname !== '/verify' && <Navbar setShowLogin={setShowLogin} />}
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
@@ -25,7 +25,7 @@ const App = () => {
           <Route path="/verify" element={<Verify/>}/>
         </Routes>
       </div>
-      {location.pathname !== '/verify' && <Footer />}
+      <Footer />
     </StoreContextProvider>
   );
 };
--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Verify.css'
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios'
 
 const Verify = () => {
-    const [searchParams,setSearchParams] = useSearchParams();
-    const succes = searchParams.get("success")
-      const orderId = searchParams.get("orderId")
+  const [searchParams] = useSearchParams()
+  const success = searchParams.get("success")
+  const orderId = searchParams.get("orderId")
+  const { url } = useContext(StoreContext)
+  const navigate = useNavigate()
+
+  const verifyPayment = async () => {
+    try {
+      const response = await axios.post(url + "/api/order/verify", {
+        success,
+        orderId
+      })
+
+      if (response.data.success) {
+        navigate("/")
+      } else {
+        navigate("/")
+      }
+    } catch (error) {
+      console.error("Verification Error:", error)
+      navigate("/")
+    }
+  }
+
+  useEffect(() => {
+    verifyPayment()
+  }, [])
+
   return (
-    <div>
-      
+    <div className='verify'>
+      <div className='spinner'></div>
     </div>
   )
 }
